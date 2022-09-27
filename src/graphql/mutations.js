8cd0caacfbbac0448f0ad878d5e610ab3548a126
jfar41 <68749301+jfar41@users.noreply.github.com>
2022-09-27 06:44:38 -0700

    used amplify push

---
+++ src/graphql/mutations.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createDoge = /* GraphQL */ `
+  mutation CreateDoge(
+    $input: CreateDogeInput!
+    $condition: ModelDogeConditionInput
+  ) {
+    createDoge(input: $input, condition: $condition) {
+      id
+      price
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateDoge = /* GraphQL */ `
+  mutation UpdateDoge(
+    $input: UpdateDogeInput!
+    $condition: ModelDogeConditionInput
+  ) {
+    updateDoge(input: $input, condition: $condition) {
+      id
+      price
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteDoge = /* GraphQL */ `
+  mutation DeleteDoge(
+    $input: DeleteDogeInput!
+    $condition: ModelDogeConditionInput
+  ) {
+    deleteDoge(input: $input, condition: $condition) {
+      id
+      price
+      createdAt
+      updatedAt
+    }
+  }
+`;
---
+++ src/graphql/queries.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getDoge = /* GraphQL */ `
+  query GetDoge($id: ID!) {
+    getDoge(id: $id) {
+      id
+      price
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listDoges = /* GraphQL */ `
+  query ListDoges(
+    $filter: ModelDogeFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listDoges(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        price
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
